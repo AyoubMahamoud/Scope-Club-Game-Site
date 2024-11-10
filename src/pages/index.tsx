89cf8e4cf5bcc5a6983f5a970b6f81e18564d740
Ayoub Mahamoud <mahamoud@usc.edu>
2024-11-10 15:54:30 -0800

    BET

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,9 @@ export default function Home() {
         <Link href="/games" className="px-4 py-2 bg-blue-500 text-white rounded-lg">
         Explore Games
         </Link>
+        <Link href="/profile" className="px-4 py-2 bg-green-500 text-white rounded-lg">
+        Profile
+        </Link>
       </div>
     </div>
   );
